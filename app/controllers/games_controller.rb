f1d2a45b445df400f5f93986d11bf786d93ffb0f
Ryan Huddleston <wryanhuddleston@gmail.com>
2021-02-11 19:52:09 +0900

    implemented game new & create, with cloudinary (pictures can now display on index and show for games)

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -14,8 +14,25 @@ class GamesController < ApplicationController
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(game_params)
+
+    @game.user = current_user
+
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
   end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :description, :availability, :photo)
+  end
+
 end
