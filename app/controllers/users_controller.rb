7339f4ea2dfb78c6fb60b15b078faf5f1be48a0d
Ryan Huddleston <wryanhuddleston@gmail.com>
2021-02-13 15:44:11 +0900

    added avatar pic, can be added to user via dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,27 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     authorize @user
   end
+
+  def edit
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    authorize @user
+
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:photo)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "users/:id", to: "users#show", as: :dashboard
 
+  resources :users, only: [ :show, :edit, :update ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
