0fb352b25c9bd560723b2979163ffd927aa0c28e
zacharygian <z.gian12@eabjm.org>
2021-02-17 10:03:12 +0900

    staging.js

--- config/webpack/staging.js
+++ config/webpack/staging.js
@@ -3,3 +3,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'staging'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+// trying with a staging.js file
